10f2a93035229d6274101317ed9cb89a5f60c0e3
SerhiyDukh1984 <s.dux2011@ukr.net>
2023-03-02 10:43:58 +0200

    Updates

---
+++ precache-manifest.ac6ec02412dfaef388647a4e6c633914.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "539e0d37585ec508806a",
+    "url": "/alter-ego-ts/static/js/0.539e0d37.chunk.js"
+  },
+  {
+    "revision": "d6be45dd29e3310ba31f",
+    "url": "/alter-ego-ts/static/css/main.fad27356.chunk.css"
+  },
+  {
+    "revision": "d6be45dd29e3310ba31f",
+    "url": "/alter-ego-ts/static/js/main.d6be45dd.chunk.js"
+  },
+  {
+    "revision": "17c5bcd18bc229e9c9f4",
+    "url": "/alter-ego-ts/static/js/2.17c5bcd1.chunk.js"
+  },
+  {
+    "revision": "00fa6bba36807568461f",
+    "url": "/alter-ego-ts/static/js/3.00fa6bba.chunk.js"
+  },
+  {
+    "revision": "abd8246f6b8098aa32da",
+    "url": "/alter-ego-ts/static/js/4.abd8246f.chunk.js"
+  },
+  {
+    "revision": "704197c4de456252f3d3",
+    "url": "/alter-ego-ts/static/js/5.704197c4.chunk.js"
+  },
+  {
+    "revision": "9808ae7abfb5973a3aa0",
+    "url": "/alter-ego-ts/static/js/6.9808ae7a.chunk.js"
+  },
+  {
+    "revision": "105dee8ac384dc8abec2",
+    "url": "/alter-ego-ts/static/js/7.105dee8a.chunk.js"
+  },
+  {
+    "revision": "c6de1fe83aa5e708e45e",
+    "url": "/alter-ego-ts/static/js/runtime~main.c6de1fe8.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/alter-ego-ts/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "58b18d6480f6a1dcb3504dcadf5072bb",
+    "url": "/alter-ego-ts/static/media/UA.58b18d64.png"
+  },
+  {
+    "revision": "31c6f5774bb5bf8bee998c7db9867da5",
+    "url": "/alter-ego-ts/index.html"
+  }
+];
